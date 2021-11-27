7d6aba777f5e6cd64ab858d5cea2607e73461b46
Erick Saraiva <erick.ssilva@bandtec.com.br>
2021-11-26 21:09:23 -0300

    PJ todo

---
+++ Banco de Dados/script-v2.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE evolve;
+
+USE evolve;
+
+CREATE TABLE usuario (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50),
+	email VARCHAR(50),
+	senha VARCHAR(50)
+);
+
+
+create table cursos (
+idCursos int auto_increment,
+categoria varchar(45),
+qtd_aulas int,
+fkUsuario int,
+primary key (idCursos),
+foreign key (fkUsuario) references usuario (id)
+);
+
+select * from usuario;
+select * from cursos;
+
+insert into cursos values
+(null, 'Movimentação', 3, null),
+(null, 'Mira', 3, null),
+(null, 'Utitários', 3, null);
